c00d0328c12d540042804e5ccccf6b040d048429
Vũ Minh Thanh <vuminhthanh8501@gmail.com>
2022-06-13 14:10:24 +0700

    gmail

---
+++ Gmail/app/src/main/java/com/example/gmail/ApiInterface.java
@@ -0,0 +1,14 @@
+package com.example.gmail;
+
+        import java.util.List;
+
+
+        import retrofit2.Call;
+        import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("inbox.json")
+    Call<List<Message>> getInbox();
+
+}
